5211ed2cf8d7ef83235f12e77a6f41b494a2c582
DESKTOP-GBVB7B1\User <wiskonsin1848@gmail.com>
2018-06-26 21:57:02 +0300

    Job done.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,74 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import shuffleArray from '../shuffleArray';
 import Tile from '../Tile/Tile';
 import './App.css';
 
+const COLORS = [
+    "green", "green", "green", "green", "blue", "blue", "blue", "blue",
+    "yellow", "yellow", "yellow", "yellow", "pink", "pink", "pink", "pink"
+];
+
 class App extends Component {
-  render() {
-      const tiles = [];
-      for (let idx = 1; idx < 17; idx++) {
-          tiles.push(<Tile key={idx} id={idx} flippedColor={idx % 2 ? "green" : "blue"}/>)
-      }
-    return (
-      <div className="App">
-        <ul>{tiles}</ul>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+
+        this.state = {
+            tiles: shuffleArray([...COLORS]),
+            selected: [],
+            correct: [],
+            count: 0
+        };
+    }
+
+    onTileClick = (i) => {
+        const {tiles, selected, correct} = this.state;
+        let {count} = this.state;
+
+        if (selected.length === 0) {
+            this.setState({
+                selected: [i],
+                count: count + 1
+            });
+        } else if (selected.length === 1) {
+            if (tiles[selected[0]] === tiles[i]) {
+                this.setState({
+                    correct: correct.concat([selected[0], i]),
+                    selected: [],
+                    count: count + 1
+                });
+            } else {
+                this.setState({selected: [selected[0], i]});
+                setTimeout(() => {
+                    this.setState({
+                        selected: [],
+                        count: count + 1
+                    })
+                }, 500);
+            }
+        }
+    };
+
+    render() {
+        const {tiles, selected, correct, count} = this.state;
+
+        return (
+            <div className="App">
+                <h1>{correct.length === tiles.length ? `Congratulations! You made: ${count} moves` : "JetRuby`s Memory Puzzle"}</h1>
+                <div className="TileGrid">
+                    {tiles.map((tile, i) => (
+                        <Tile
+                            key={i}
+                            color={tile}
+                            isSelected={selected.includes(i)}
+                            isCorrect={correct.includes(i)}
+                            onSelect={() => this.onTileClick(i)}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,33 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './Tile.css';
 
-class Tile extends Component {
-    constructor(props) {
-        super(props);
+const Tile = ({color, isSelected, isCorrect, onSelect}) => {
+    const classes = (isSelected || isCorrect) ? `Tile Tile-${color}` : "Tile";
+    return (
+        <div
+            className={classes}
+            onClick={() => {
+                if (!isSelected && !isCorrect) {
+                    onSelect();
+                }
+            }}
+        />
+    )
+};
 
-        this.state = {
-            flipped: false,
-            flippedColor: this.props.flippedColor
-        }
-    }
-
-    flipTile = () => {
-      this.setState({flipped: true})
-    };
-
-    render() {
-        const isFlipped = this.state.flipped;
-        const classes = this.state.flippedColor;
-
-        return (
-            <li
-                className={isFlipped ? `Tile Tile-${classes}` : "Tile"}
-                id={`Tile-${this.props.id}`}
-                onClick={this.flipTile}
-            />
-        );
-    }
-}
 
 export default Tile;
